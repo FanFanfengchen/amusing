eae30d0a9f32b496232a22721087d6d4d8e52713
FanFanfengchen <133505922+FanFanfengchen@users.noreply.github.com>
2025-02-22 18:54:17 +0800

    Update 好玩的2.6.py
    修改了部分时间，增加了部分错误剧情，修改了并增加了部分剧情，还有，再见了

--- 好玩的2.5.py
+++ 好玩的2.5.py
@@ -11,7 +11,7 @@ while not task_completed:  # 外层循环，用于整个程序的重来机制
     if a == "抛掷":  # 使用比较运算符比较输入内容
         print("你抛掷了一枚硬币")
         time.sleep(2)
-        print("反向落入水中")
+        print("这枚硬币反向落入水中")
         time.sleep(1)
         print("产生涟漪")
         time.sleep(1.25)
@@ -36,6 +36,8 @@ while not task_completed:  # 外层循环，用于整个程序的重来机制
             if b == "锚点":
                 time.sleep(1.5)
                 print("你已选中最近的锚点")
+                time.sleep(2.5)
+                print("你接触到了锚点")
                 time.sleep(2)
 
                 while not task_completed:  # 第二层循环，用于验证代码
@@ -46,7 +48,7 @@ while not task_completed:  # 外层循环，用于整个程序的重来机制
                         print("验证通过")
                         time.sleep(1)
                         print("恭迎您，陛下")  # 陪伴您最长久的灰发女巫，夏绿蒂
-                        time.sleep(3)
+                        time.sleep(4)
 
                         while not task_completed:  # 第三层循环，用于验证关键词
                             print("验证关键词：")
@@ -54,7 +56,7 @@ while not task_completed:  # 外层循环，用于整个程序的重来机制
                             if d == "白维":
                                 time.sleep(0.3)
                                 print("验证通过，第一阶段解锁")
-                                time.sleep(1)
+                                time.sleep(2)
 
                                 while not task_completed:  # 第四层循环，用于验证关键词
                                     print("验证关键词：")
@@ -62,15 +64,15 @@ while not task_completed:  # 外层循环，用于整个程序的重来机制
                                     if e == "黑色蔷薇":
                                         time.sleep(0.3)
                                         print("验证通过，第二阶段解锁")
-                                        time.sleep(1)
+                                        time.sleep(2)
                                         print("最终阶段即将展开")
-                                        time.sleep(1)
+                                        time.sleep(1.5)
                                         print(
                                             "警告：启用该权能碎片将会导致灵界海洋暴动，产生多次回响，后果难以预知"
                                         )
-                                        time.sleep(2)
+                                        time.sleep(2.5)
                                         print("如果执意发动权能，请通过最终验证")
-                                        time.sleep(2)
+                                        time.sleep(2.5)
 
                                         while not task_completed:  # 第五层循环，用于最终验证
                                             print("验证关键词：")
@@ -78,74 +80,88 @@ while not task_completed:  # 外层循环，用于整个程序的重来机制
                                             if f == "再见了，夏绿蒂。" and "再见了，夏绿蒂":
                                                 time.sleep(0.3)
                                                 print("最终验证结束")
-                                                time.sleep(0.6)
-                                                print("黄金大权，权能展开")
                                                 time.sleep(1)
+                                                print("黄金大权，权能展开")
+                                                time.sleep(1.2)
                                                 print("灵界海洋深处，传出无垠回响。")
-                                                time.sleep(2)
+                                                time.sleep(2.6)
                                                 print("现实位面")
                                                 time.sleep(2)
                                                 print(
                                                     "你睁开双眼，眼瞳深处映照出黄金色的辉煌光芒"
                                                 )
-                                                time.sleep(3)
+                                                time.sleep(3.5)
                                                 print(
                                                     "你伫立于此，却如同端坐在黄金宫殿的恢弘王座之上"
                                                 )
-                                                time.sleep(3)
+                                                time.sleep(3.7)
                                                 print("仅有数息的时间，黄金大全在握")
-                                                time.sleep(2)
+                                                time.sleep(2.4)
                                                 print("你仅抬起手")
                                                 time.sleep(2)
                                                 print("啪嗒~轻轻地打了个响指")
-                                                time.sleep(1)
+                                                time.sleep(1.8)
                                                 print("召唤仪式99%......")
-                                                time.sleep(1)
+                                                time.sleep(1.2)
                                                 print("中止......")
                                                 time.sleep(3)
                                                 print(
                                                     "水池中即将满溢而出的黑泥，悄无声息的在黄金色的辉光之中湮灭而去，洒落作遍地粉尘。"
                                                 )
-                                                time.sleep(5)
-                                                print("任务完成！")
+                                                time.sleep(5.6)
+                                                print("结束开溜")
                                                 task_completed = True  # 设置任务完成标志
                                             else:
                                                 time.sleep(0.3)
-                                                print("错误！")
+                                                print("不对！")
                                                 time.sleep(1)
                                                 print("最终验证结束")
                                                 time.sleep(1.5)
                                                 print(
-                                                    "深呼吸，好好想想陪您最长久的灰发女巫是谁。"
+                                                    "深呼吸，好好回想陪您最长久的灰发女巫是谁。"
                                                 )
+                                                time.sleep(2.5)
                                                 continue  # 返回到最终验证的if前重新开始
                                     else:
                                         time.sleep(0.3)
                                         print("验证失败，无法启动第二阶段的权能")
                                         time.sleep(2)
                                         print("别让黑色蔷薇失望啊！")
-                                        time.sleep(3)
+                                        time.sleep(3.6)
                                         print("我的陛下")
+                                        time.sleep(2)
                                         continue  # 返回到第四层循环的if前重新开始
                             else:
                                 time.sleep(0.3)
                                 print("验证失败，无法启动第一阶段的权能")
-                                time.sleep(1.5)
+                                time.sleep(2)
                                 print("不会看了小凡就来验证了吧？")
-                                time.sleep(3)
+                                time.sleep(2.5)
                                 print("看看原作再来")
+                                time.sleep(2.5)
                                 continue  # 返回到第三层循环的if前重新开始
                     else:
                         time.sleep(0.3)
                         print("验证失败，你不是他")
+                        time.sleep(2)
                         continue  # 返回到第二层循环的if前重新开始
             else:
                 time.sleep(2)
-                print("请自行摸索")
+                print("前往你的目的地")
+                time.sleep(2)
+                print("糟了，迪拉克之海好像有点大")
+                time.sleep(2)
+                print("再找找别的锚点吧！")
+                time.sleep(1)
                 continue  # 返回到第一层循环的if前重新开始
     else:
-        time.sleep(3)
+        time.sleep(2.3)
         print("达成成就：硬币呢？")
+        time.sleep(2)
+        print("抛掷吧！不要忘记开始")
+        time.sleep(3)
+        print("因为，还有重要之人需要保护。")
+        time.sleep(1)
         continue  # 返回到外层循环的if前重新开始
 
 # 如果用户成功完成任务，程序继续执行后续内容
@@ -181,7 +197,6 @@ time.sleep(1)
 print(
     "她突然想到刚刚感受到的奇怪的记忆，望着白维的背影，只感觉突然间那么的似曾相识，像是另一个人。"
 )
-time.sleep(3)
 g = input()
 if g == "怎么了？" and "怎么了":
     time.sleep(1)
